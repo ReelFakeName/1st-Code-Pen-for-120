a9569e006100972f21c408450042efc302a5a16a
ReelFakeName <71346555+ReelFakeName@users.noreply.github.com>
2021-02-09 19:01:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+var name = prompt ("What Is Your Name")
+var color = prompt ("What is your Favorite Color")
+document.body.querySelector(".message").innerHTML="Hello My Name Is "+name+". I Like The Color "+color;
